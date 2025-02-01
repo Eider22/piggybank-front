be8e582d054630f6cc7b0e67576759a801651db2
unknown <eidercardona22@gmail.com>
2025-02-01 18:51:16 -0500

    feature - Se crea el componente footer - Se hacen algunos ajustes de estilo en la página see-funds(ver fondos)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { FundOperationsModule } from './features/fund-operations/fund-operations.module';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FundOperationsModule],
+  imports: [BrowserModule, AppRoutingModule, FundOperationsModule, SharedModule],
   providers: [provideHttpClient(withInterceptors([authInterceptor]))],
   bootstrap: [AppComponent],
 })
