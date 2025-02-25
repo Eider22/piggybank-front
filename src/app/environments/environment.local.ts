d3af50d875f2bd2be669403d0fbe8dbc024ad598
unknown <eidercardona22@gmail.com>
2025-02-25 12:07:31 -0500

    chore - Se cambian iconos de color

--- src/app/environments/environment.local.ts
+++ src/app/environments/environment.local.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080/api/v1/',
-  // apiUrl: 'http://192.168.1.3:8080/api/v1/',
+  // apiUrl: 'http://192.168.1.7:8080/api/v1/',
   // apiUrl: 'http://18.221.37.210:8080/api/v1/',
   clientUsername: 'client',
   clientPassword: 'client',
